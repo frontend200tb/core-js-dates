35d51501604080590ad0c28b1d8676e89e1016fe
frontend200tb <frontend200tb@mail.ru>
2024-02-29 08:05:05 +0300

    feat: 3 getDayName

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -54,8 +54,18 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const week = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const time = new Date(date);
+  return week[time.getDay()];
 }
 
 /**
